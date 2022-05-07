360d8c997319888cc20d19d3d01bd5b1db229982
wyller <wyller22@gmail.com>
2022-05-07 17:27:06 -0300

    Small crop_info fixes and type hints.

--- controller/crop_info.py
+++ controller/crop_info.py
@@ -4,63 +4,70 @@ from model.units import CropBox, Pixels
 
 class CropInfo:
     def __init__(self, view, gif_info: GifInfo):
-        self._start = view['-START-']
-        self._end = view['-END-']
-        self._size = view['-BOX-']
-        self._reset_btn = view['-RESET_BTN-']
-        self._crop_btn = view['-CROP_BTN-']
-        self._gif_size = gif_info.size
-        self._resize_factor = gif_info.resize_factor
+        self._start_txt = view['-START-']
+        self._end_txt = view['-END-']
+        self._size_txt = view['-BOX-']
+        self._btns = (view['-RESET_BTN-'], view['-CROP_BTN-'])
+        self._resize = gif_info.resize_factor
 
-    def update(self, crop_box: CropBox):
-        crop = self._real_box_size(crop_box)
-        if (crop.x1 or crop.y1) is None:
-            self._update_start(crop)
-            return
-        if crop.x0 > crop.x1 and crop.y0 > crop.y1:
-            crop = _invert_xy(crop)
-        self._update_end(crop)
+
+    def update(self, box: CropBox):
+        box = _fix_inverted_box(box)
+        crop = _real_box_size(self._resize, box)
+        if (crop.x1 or crop.y1) is not None:
+            self._set_all_info(crop)
+        self._set_start_info(crop)
+        return
+
 
     def clear(self):
-        self._start.update('None')
-        self._end.update('None')
-        self._size.update('None')
-        self._update_btns(disabled=True)
+        self._start_txt.update('None')
+        self._end_txt.update('None')
+        self._size_txt.update('None')
+        self._set_btns_state(disabled=True)
 
 
-    def _update_start(self, box: CropBox):
-        self._start.update(
-            f"{box.x0} / {box.y0}",
-            text_color='yellow'
-        )
-        self._update_btns(disabled=True)
+    def _set_start_info(self, box: CropBox):
+        self._set_btns_state(disabled=True)
+        start_txt = f"{box.x0} px / {box.y0} px"
+        self._start_txt.update(start_txt, text_color='yellow')
 
-    def _update_end(self, box: CropBox):
-        size = _crop_size(box)
-        self._start.update(f"{box.x0} / {box.y0}",
-                           text_color='white')
-        self._end.update(f"{box.x1} / {box.y1}")
-        self._size.update(f"{size.x}x{size.y} px")
-        self._update_btns(disabled=False)
 
-    def _update_btns(self, disabled: bool):
-        self._reset_btn.update(disabled=disabled)
-        self._crop_btn.update(disabled=disabled)
+    def _set_all_info(self, box: CropBox):
+        self._set_btns_state(disabled=False)
+        start, end, new_size = _format_all_info(box)
+        self._start_txt.update(start, text_color='white')
+        self._end_txt.update(end)
+        self._size_txt.update(new_size)
 
-    def _real_box_size(self, box: CropBox):
-        box = [int(n / self._resize_factor)
-                for n in box if n is not None]
-        if len(box) == 2:
-            return CropBox(box[0], box[1], None, None)
+
+    def _set_btns_state(self, disabled: bool):
+        for btn in self._btns:
+            btn.update(disabled=disabled)
+
+
+def _real_box_size(resize: float, box: CropBox):
+    if resize == 1.0:
+        return box
+    box = [int(n / resize) for n in box if n is not None]
+    if len(box) == 4:
         return CropBox(*box)
+    return CropBox(box[0], box[1], None, None)
 
 
-def _invert_xy(crop: CropBox) -> CropBox:
-    start = crop.x1, crop.y1
-    end = crop.x0, crop.y0
+def _format_all_info(box: CropBox) -> tuple[str, str, str]:
+    new_w = abs(box.x1 - box.x0 + 1)
+    new_h = abs(box.y1 - box.y0 + 1)
+    size = Pixels(new_w, new_h)
+    start = f"{box.x0} px / {box.y0} px"
+    end = f"{box.x1} px / {box.y1} px"
+    new_size = f"{size.x}x{size.y} px"
+    return start, end, new_size
+
+
+def _fix_inverted_box(box: CropBox) -> CropBox:
+    if box.x0 < box.x1 and box.y0 < box.y1:
+        return box
+    start = box.x1, box.y1
+    end = box.x0, box.y0
     return CropBox(*start, *end)
-
-def _crop_size(crop: CropBox) -> Pixels:
-    width = abs(crop.x1 - crop.x0 + 1)
-    height = abs(crop.y1 - crop.y0 + 1)
-    return Pixels(width, height)
