8258eda16e23486720bb9dab934badc085cfecf8
wyller <wyller22@gmail.com>
2022-05-08 02:27:29 -0300

    Cleaner GifInfo class.

--- model/gif_info.py
+++ model/gif_info.py
@@ -6,17 +6,12 @@ from model.units import Pixels
 
 class GifInfo:
     def __init__(self, file):
-        _gif_img = Image.open(file)
-        _screen = _get_monitor_area()
+        gif = Image.open(file)
+        size = Pixels(*gif.size)
         self.gif_file = file
-        self.size = Pixels(*_gif_img.size)
-        self.n_frames: int = _gif_img.n_frames
-        if self.size.x <= _screen.x and self.size.y <= _screen.y:
-            self.resize_factor = 1.0
-        else:
-            factor_x: float = (_screen.x / self.size.x)
-            factor_y: float = (_screen.y / self.size.y)
-            self.resize_factor = min(factor_x, factor_y)
+        self.size = size
+        self.resize_factor = _get_resize_factor(size)
+        self.n_frames: int = gif.n_frames
 
     @property
     def display_size(self) -> Pixels:
@@ -29,14 +24,20 @@ class GifInfo:
         return Pixels(width, height)
 
 
-def _get_monitor_area(factor=0.7) -> Pixels:
+def _get_resize_factor(size: Pixels, monitor_area=0.7) -> float:
     """
     Gets the available monitor display
     resolution for the gif object.
     """
-    w, h = 10000, 10000
-    for m in get_monitors():
-        if m.width < w and m.height < h:
-            w, h = m.width, m.height
-    w, h = int(w * factor), int(h * factor),
-    return Pixels(w, h)
+    w, h = 100000, 100000
+    for mon in get_monitors():
+        if mon.width < w and mon.height < h:
+            w, h = mon.width, mon.height
+    w, h = int(w * monitor_area), int(h * monitor_area)
+    area = Pixels(w, h)
+    if size.x <= area.x and size.y <= area.y:
+        return 1.0
+    else:
+        x_resize: float = (area.x / size.x)
+        y_resize: float = (area.y / size.y)
+        return min(x_resize, y_resize)
