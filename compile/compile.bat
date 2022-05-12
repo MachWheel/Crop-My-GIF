761bd67e9b3d4e5f10c7bc2f6955ba02688c5cbc
wyller <wyller22@gmail.com>
2022-05-12 18:03:51 -0300

    Updated requirements; Updated compile script

--- compile/compile.bat
+++ compile/compile.bat
@@ -1,16 +1,19 @@
 @ECHO OFF
-:: This batch file compiles Crop-My-GIF application
-:: The generated app is in the ./dist folder
-:: First make sure to "pip install -r requirements.txt"
-TITLE Crop-My-GIF-v1 Compiler
+TITLE Crop-My-GIF Compiler v2
 ECHO:
-ECHO Crop-My-GIF-v1 Compiler
+ECHO Crop-My-GIF Compiler v2
 ECHO:
-ECHO PRESS ANY KEY TO START COMPILING
+ECHO This batch script compiles Crop-My-GIF application
+ECHO:
+ECHO Make sure to proper configure the project virutalenv first.
+ECHO:
+ECHO You should run this script inside the project virtualenv to avoid problems.
+ECHO:
+ECHO The generated app will be in the ./dist folder
 ECHO:
 PAUSE
 MKDIR dist
-pyinstaller -w --onefile ..\main.py --icon app.ico --name Crop-My-GIF-v1
+pyinstaller -w --onefile ..\main.py --icon app.ico --name Crop-My-GIF
 ECHO:
 ECHO DONE! PRESS ANY KEY TO OPEN THE OUTPUT FOLDER.
 ECHO:
