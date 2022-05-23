791c9f04b2607b9bc15ef658f3d0db1090f2a987
wyller <wyller22@gmail.com>
2022-05-23 19:09:26 -0300

    Units docs

--- model/units.py
+++ model/units.py
@@ -1,4 +1,11 @@
+"""
+This module holds named tuples that measure pixel units:
+
+    * Pixels - x and y pixel coordinates as int values
+    * CropBox - x0, y0 and x1, y1 pixel coordinates as int values
+"""
 from collections import namedtuple
+
 
 Pixels = namedtuple("Pixels", "x y")
 CropBox = namedtuple("CropBox", "x0 y0 x1 y1")
