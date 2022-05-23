9c6e2ce5c85536e4b6f5909d2f2295c021a6a309
wyller <wyller22@gmail.com>
2022-05-22 21:19:01 -0300

    GifInfo docs

--- model/gif_info.py
+++ model/gif_info.py
@@ -6,6 +6,12 @@ from .units import Pixels
 
 class GifInfo:
     def __init__(self, file):
+        """
+        Initializes a new GifInfo object.
+
+        :param file: GIF file path
+        :type file: str
+        """
         gif = Image.open(file)
         size = Pixels(*gif.size)
         self.gif_file = file
@@ -15,12 +21,27 @@ class GifInfo:
 
     @property
     def display_size(self) -> Pixels:
+        """
+        :return: Current displaying GIF width and height
+            as a named tuple of Pixels(x, y)
+        :rtype: model.units.Pixels
+        """
         width = int(self.size.x * self.resize_factor)
         height = int(self.size.y * self.resize_factor)
         return Pixels(width, height)
 
 
 def _get_resize_factor(gif_size: Pixels) -> float:
+    """
+    DESCRIPTION
+
+    :param gif_size: GIF file width and height as a
+        named tuple of Pixels(x, y)
+    :type gif_size: model.units.Pixels
+
+    :return: View resize factor as a float value
+    :rtype: float
+    """
     usable: Pixels = _usable_area()
     if gif_size.x <= usable.x and gif_size.y <= usable.y:
         return 1.0
@@ -29,7 +50,16 @@ def _get_resize_factor(gif_size: Pixels) -> float:
         y_resize: float = (usable.y / gif_size.y)
         return min(x_resize, y_resize)
 
-def _usable_area(default=0.7):
+def _usable_area(default: float = 0.7) -> Pixels:
+    """
+    :param default: Optional: the default display
+        usable area as a float value.
+    :type default: flot
+
+    :return: The usable width and height of the smallest
+        monitor as a named tuple of Pixels(x, y)
+    :rtype: model.units.Pixels
+    """
     w = h = 10 ** 10
     for monitor in screeninfo.get_monitors():
         mw, mh = monitor.width, monitor.height
