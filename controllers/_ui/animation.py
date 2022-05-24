4780b2576c58a51010c409566801f5759cdebde8
wyller <wyller22@gmail.com>
2022-05-24 15:30:38 -0300

    Animation docs

--- controllers/_ui/animation.py
+++ controllers/_ui/animation.py
@@ -7,12 +7,29 @@ from model.gif_info import GifInfo
 
 
 class Animation:
+    """
+    Controls and generates PySimpleGUI Window
+    events related to the currently displayed
+    GIF animation.
+    """
     def __init__(self, view: Window, gif_info: GifInfo):
+        """
+        Initializes a new Animation object.
+
+        :param view: PySimpleGUI Window to control
+        :type view: sg.Window
+
+        :param gif_info: Object containing the GIF file information
+        :type gif_info: model.GifInfo
+        """
         self._view = view
         self._info = gif_info
         self._stop_animation = False
 
     def start(self):
+        """
+        Starts a thread that generates animation events.
+        """
         frames_thread = Thread(
             target=self._frame_events,
             args=(
@@ -23,7 +40,21 @@ class Animation:
         )
         frames_thread.start()
 
-    def _frame_events(self, n_frames: int, stop):
+    def _frame_events(self, n_frames: int, stop) -> None:
+        """
+        Needs to be called within a thread.
+
+        Generates a PySimpleGUI Window event tuple:
+        ("NextFrame", n) every 0.01 second,
+
+        where "n" is the current gif frame index.
+
+        :param n_frames: Total number of frames
+        :type n_frames: int
+
+        :param stop: Returns _stop_animation flag state
+        :type stop: () -> bool
+        """
         print('Animation thread started.')
         frame = 0
         while True:
@@ -35,10 +66,12 @@ class Animation:
             self._view.write_event_value('NextFrame', frame)
 
     def hide_and_pause(self):
+        """Hides current view and stops the animation thread."""
         self._view.hide()
         self._stop_animation = True
 
     def unhide_and_resume(self):
+        """Shows currently hidden view and restarts the animation thread."""
         self._view.un_hide()
         self._stop_animation = False
         self.start()
