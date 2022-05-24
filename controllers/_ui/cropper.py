06b649be20c3809f5c72d210bf4c912ebe60a5af
wyller <wyller22@gmail.com>
2022-05-24 15:11:16 -0300

    Cropper docs

--- controllers/_ui/cropper.py
+++ controllers/_ui/cropper.py
@@ -11,14 +11,36 @@ from controllers import _msgs
 
 
 class Cropper:
-
+    """
+    Controller responsible for cropping and exporting
+    a given GIF file.
+    """
     def __init__(self, info: model.GifInfo, box: model.units.CropBox, preserve_fps: bool):
+        """
+        Initializes a new Cropper object.
+
+        :param info: Object containing the GIF file information
+        :type info: model.GifInfo
+
+        :param box: selected crop coordinates as
+            a named tuple: CropBox(x0, y0, x1, y1)
+        :type box: model.units.CropBox
+
+        :param preserve_fps: Preserves the input frame rate if True
+        :type preserve_fps: bool
+        """
         self._info = info
         self._box = box
         self._preserve_fps = preserve_fps
         self._export_fps = 10.0 if preserve_fps else 20.0
 
     def export_gif(self) -> str | None:
+        """
+        Exports a cropped GIF file according to current instance values.
+
+        :returns: The output file path if successful
+        :rtype: str | None
+        """
         in_name = splitext(self._info.gif_file)[0]
         output = f"{in_name}_CROP.gif"
         if not self._run_export(output):
@@ -29,12 +51,25 @@ class Cropper:
         return self._run_export(output)
 
     def _run_export(self, output: str) -> str | None:
+        """
+        Submits the GIF file writing task as a thread,
+        displaying its progress in a popup window.
+
+        :returns: The output file path if successful
+        :rtype: str | None
+        """
         with ThreadPoolExecutor() as e:
             task = e.submit(self._write_task, output)
             _show_running(task)
             return task.result()
 
     def _write_task(self, output) -> str | None:
+        """
+        Writes the cropped GIF file output.
+
+        :returns: The output file path if successful
+        :rtype: str | None
+        """
         obj = VideoFileClip(self._info.gif_file).subclip(0)
         obj = crop(obj, *self._box)
         fps = self._export_fps
@@ -44,7 +79,12 @@ class Cropper:
             return output
         return None
 
-    def _fix_export_fps(self, output):
+    def _fix_export_fps(self, output) -> None:
+        """
+        Sets cropper _export_fps value as the input
+        frame rate by reading the first output number
+        of frames.
+        """
         in_n_frames = self._info.n_frames
         out_n_frames = 0
         for _ in iio.imiter(output):
@@ -53,7 +93,14 @@ class Cropper:
         self._export_fps = fps
 
 
-def _show_running(task: Future[str]):
+def _show_running(task: Future[str]) -> None:
+    """
+    Displays the running exporting task progress
+    in a popup window.
+
+    :param task: Running export task
+    :type task: Future[str]
+    """
     i, reload_i, bar_end = 0, 199, 200
     view = views.PROGRESS(importing=False, bar_end=bar_end)
     while task.running():
