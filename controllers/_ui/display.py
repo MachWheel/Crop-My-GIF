29fd0a407ed547ec3fb2c7a32dd790ef895c8abe
wyller <wyller22@gmail.com>
2022-05-24 14:01:47 -0300

    Display docs

--- controllers/_ui/display.py
+++ controllers/_ui/display.py
@@ -1,8 +1,23 @@
+import PySimpleGUI as sg
+
 import model
 from .frames import Frames
 
 class Display:
-    def __init__(self, view, gif_frames: Frames):
+    """
+    Controller responsible for drawing GIF frames
+    and user selections.
+    """
+    def __init__(self, view: sg.Window, gif_frames: Frames):
+        """
+        Initializes a new Display object.
+
+        :param view: PySimpleGUI Window to control
+        :type view: sg.Window
+
+        :param gif_frames: Object containing GIF frames
+        :type gif_frames: controllers._ui.Frames
+        """
         self.selected = None
         self._graph = view['-GRAPH-']
         self._location = (0, 0)
@@ -13,14 +28,26 @@ class Display:
         self.draw_gif_frame(0)
         return
 
-    def draw_gif_frame(self, index):
+    def draw_gif_frame(self, index: int) -> None:
+        """
+        Displays a GIF frame according to index value
+
+        :param index: Current frame number
+        :type index: int
+        """
         self._graph.delete_figure(self._current_frame)
         self._current_frame = self._graph.draw_image(
             data=self._frames.loaded[index],
             location=self._location
         )
 
-    def draw_selection(self, selection: model.Selection):
+    def draw_selection(self, selection: model.Selection) -> None:
+        """
+        Draws a given user selection over the display
+
+        :param selection: Object that holds user selection
+        :type selection: model.Selection
+        """
         self._current_color = int(not self._current_color)
         self._graph.delete_figure(self.selected)
         if None in selection.end:
@@ -28,14 +55,28 @@ class Display:
         else:
             self._draw_box(selection)
 
-    def _draw_start(self, selection):
+    def _draw_start(self, selection: model.Selection) -> None:
+        """
+        Draws a dot over the display representing
+        the beginning of user selection.
+
+        :param selection: Object that holds user selection
+        :type selection: model.Selection
+        """
         self.selected = self._graph.draw_point(
             point=selection.start,
             size=10,
             color=self._colors[self._current_color]
         )
 
-    def _draw_box(self, selection):
+    def _draw_box(self, selection: model.Selection) -> None:
+        """
+        Draws a box over the display representing
+        user selected crop area.
+
+        :param selection: Object that holds user selection
+        :type selection: model.Selection
+        """
         self.selected = self._graph.draw_rectangle(
             top_left=selection.start,
             bottom_right=selection.end,
